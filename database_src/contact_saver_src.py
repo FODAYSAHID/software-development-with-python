9c2e57d09b039245a15cfb08679289f0ff304c38
Foday S.N Kamara <fodaysnkamara@gmail.com>
2024-07-17 00:14:51 +0200

    Update contact_saver_src.py

--- database_src/contact_saver_src.py
+++ database_src/contact_saver_src.py
@@ -3,9 +3,6 @@ import sqlite3
 try:
     conn = sqlite3.connect("contact_saver_software.db")
     c = conn.cursor()
-except Exception as e:
-    print(str(e))
-else:
     c.executescript(
             """CREATE TABLE CONTACTS(
                 ContactId INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,6 +14,10 @@ else:
     c.execute("INSERT INTO CONTACTS(ContactName, ContactNumber) VALUES('FODAY S.N KAMARA', '+232-88-76-77-95')")
     conn.commit()
     conn.close()
+except Exception as e:
+    print(str(e))
+
+    
     
 
 
